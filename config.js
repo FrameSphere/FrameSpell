c05cb49444725b0a0ea932e9cefcbd91d72e03b7
KarolP-tech <karol.paschek@ferienwohnung-zornheim.de>
2026-02-08 00:29:30 +0100

    config fixes

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 // API Configuration für Cloudflare Workers Backend
 // WICHTIG: Setze hier deine Worker URL ein!
 
-const API_BASE_URL = 'https:// rechtschreibe-api.karol-paschek.workers.dev';
+const API_BASE_URL = 'https://rechtschreibe-api.karol-paschek.workers.dev';
 
 // Oder bei Custom Domain:
 // const API_BASE_URL = 'https://api.deine-domain.com';
